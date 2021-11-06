b89c8dd3551581964a98e0c87ac5fe842b7442cc
tomassmagalhaes <1151182@isep.ipp.pt>
2021-11-06 14:53:10 +0000

    First Rules with CF

--- src/main/java/org/engcia/cf/model/Evidence.java
+++ src/main/java/org/engcia/cf/model/Evidence.java
@@ -2,6 +2,15 @@ package org.engcia.cf.model;
 
 public class Evidence extends FactCF{
 
+	public static final String PRECO_PRIORITARIO = "O preço é o fator prioritário em relação ao consume e design";
+	public static final String CONSUMO_PRIORITARIO = "O consumo é o fator prioritário em relação ao design";
+	public static final String DESIGN_PRIORITARIO = "O design é o fator prioritário";
+	public static final String QUAL_O_PRECO = "Disposto a pagar mais de 10.000€";
+	public static final String ANTIGUIDADE_KILOMETRAGEM = "Prefere um carro mais antigo ao invés de um com mais kilometragem";
+	public static final String IMPOSTO_VEICULO = "Quer ter em conta o imposto anual";
+	public static final String ESTADO_VEICULO = "O estado do veiculo é relevante";
+	public static final String COMBUSTIVEIS_FOSSEIS = "Prefere combustíveis fósseis em vez de um carro elétrico";
+
 	public Evidence(double cf, String description, String value) {
 		super(cf, description, value);
 	}
